382eb2a77da60d073d05e7603e9b016f4a19a0bf
May (PC) <89427486+Towodile@users.noreply.github.com>
2025-07-15 15:19:00 +0200

    Delete SelectMusicTrackAction.java

--- src/main/java/zone/towo/musicconfig/screen/SelectMusicTrackAction.java
+++
@@ -1,9 +0,0 @@
-package zone.towo.musicconfig.screen;
-
-import zone.towo.musicconfig.music.MusicTrack;
-
-import java.util.List;
-
-public interface SelectMusicTrackAction {
-    void processSelection(List<MusicTrack> tracks);
-}
